e94a41dfedf8c1a6391ca4b96a771e9ab071842e
wallerand grevet <you@example.com>
2020-05-08 10:00:54 +0200

    Creation de la barre de recherche de film

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './containers/app';
+
+const API_KEY ="4d3ab8c9e76925ba3947c4632098fae0";
 
 ReactDOM.render(
   <React.StrictMode>
